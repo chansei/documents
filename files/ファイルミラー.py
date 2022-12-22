342df0f8d549e85e62e1e8b149da02e79c6e3f82
chansei <seinosuke.train@gmail.com>
2022-12-22 13:28:16 +0900

    コピーツール追加

---
+++ files/ファイルミラー.py
@@ -0,0 +1,65 @@
+import glob
+import logging
+import os
+import shutil
+import traceback
+from tkinter import filedialog
+
+############################
+BUILD_VER = '1.0.1'
+############################
+
+logger = logging.getLogger()
+
+def main():
+    # dstが基準となるフォルダー(空のフォルダー)
+    # srcが検索対象とするフォルダー
+    
+    dst = filedialog.askdirectory(title='基準となるフォルダーを選択してください') 
+    src = filedialog.askdirectory(title='検索対象のフォルダーを選択してください')
+
+    files = os.listdir(dst)
+    dst_dirs = [f for f in files if os.path.isdir(os.path.join(dst, f))]
+
+    print(f'基準フォルダー：{dst}')
+    print(f'検索対象フォルダー：{src}')
+    print(color(f'{len(dst_dirs)}個のフォルダーが見つかりました'))
+    logger.info(f'基準フォルダー：{dst}, 検索対象フォルダー：{src}, {len(dst_dirs)}個のフォルダー')
+
+    print(f'出力先のフォルダーを作成中...')
+    try:
+        out_dir = dst+'\\'+'@出力'
+        os.mkdir(out_dir)
+        print(f'完了しました')
+    except Exception:
+        print(f'既に存在しているか、アクセス権限がありません')
+        logger.error('出力先フォルダー作成失敗',exc_info=True)
+
+    for i, dst_dir in enumerate(dst_dirs):
+        print(color(f'{str(len(dst_dirs))}個中{str(i+1)}個目のフォルダーを検索中...'))
+        for cand in glob.glob(src+'/**/', recursive=True):
+            cand_dir_name_list = cand.split('\\')
+            cand_dir_name = cand_dir_name_list[len(cand_dir_name_list)-2]
+            if os.path.isdir(cand) and dst_dir==cand_dir_name:
+                print(f'コピーしています...')
+                logger.info(f"コピー元：{cand}, コピー先：{out_dir}\{dst_dir}")
+                try:
+                    print(cand)
+                    shutil.copytree(cand, out_dir+'\\'+dst_dir)
+                    print(f'完了しました')
+                except Exception:
+                    print(f'エラーが発生し、その内容はログに出力されました')
+                    logger.error('コピー失敗',exc_info=True)
+            else:
+                (f'フィルダーが見つかりませんでした')
+
+    _ = input('何かキーを押すと終了します...')
+
+def color(txt): # 文字の色づけ
+    return '\033[33m'+txt+'\033[0m'
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG, filename="fp.log", format="%(asctime)s %(levelname)-7s %(message)s")
+    print(f'***CopyRight 2022 Chansei***')
+    print(f'***Build Version {BUILD_VER}***')
+    main()
