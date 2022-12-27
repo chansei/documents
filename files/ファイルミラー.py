09b9ca8548771aae2cc0bf5ac6d2648b27a9397e
Chansei <seinosuke.train@gmail.com>
2022-12-28 01:15:06 +0900

    v1.0.2

--- files/ファイルミラー.py
+++ files/ファイルミラー.py
@@ -3,10 +3,11 @@ import logging
 import os
 import shutil
 import traceback
+from datetime import datetime
 from tkinter import filedialog
 
 ############################
-BUILD_VER = '1.0.1'
+BUILD_VER = '1.0.2'
 ############################
 
 logger = logging.getLogger()
@@ -18,11 +19,18 @@ def main():
     dst = filedialog.askdirectory(title='基準となるフォルダーを選択してください') 
     src = filedialog.askdirectory(title='検索対象のフォルダーを選択してください')
 
+    if len(dst)==0 or len(src)==0:
+        print(f'基準フォルダーまたは検索対象フォルダーが空です')
+        logger.error('フォルダー未設定')
+        _ = input('何かキーを押すと終了します...')
+        return
+
     files = os.listdir(dst)
     dst_dirs = [f for f in files if os.path.isdir(os.path.join(dst, f))]
 
     print(f'基準フォルダー：{dst}')
     print(f'検索対象フォルダー：{src}')
+
     print(color(f'{len(dst_dirs)}個のフォルダーが見つかりました'))
     logger.info(f'基準フォルダー：{dst}, 検索対象フォルダー：{src}, {len(dst_dirs)}個のフォルダー')
 
@@ -51,7 +59,8 @@ def main():
                     print(f'エラーが発生し、その内容はログに出力されました')
                     logger.error('コピー失敗',exc_info=True)
             else:
-                (f'フィルダーが見つかりませんでした')
+                logger.info(f"存在しません：{dst_dir}")
+                print(f'フィルダーが見つかりませんでした')
 
     _ = input('何かキーを押すと終了します...')
 
@@ -59,7 +68,10 @@ def color(txt): # 文字の色づけ
     return '\033[33m'+txt+'\033[0m'
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG, filename="fp.log", format="%(asctime)s %(levelname)-7s %(message)s")
+    log_file_name = '{:%Y-%m-%d-%H%M%S}'.format(datetime.now())+'.log'
+    logging.basicConfig(level=logging.DEBUG, filename=log_file_name, format="%(asctime)s %(levelname)-7s %(message)s")
     print(f'***CopyRight 2022 Chansei***')
     print(f'***Build Version {BUILD_VER}***')
+    print(f'このソフトウェアはMITライセンスのもと公開されています')
+    print(color(f'ログファイル：{log_file_name}'))
     main()
